ea6d4ab2614a2841433f1896a8f6e117a0389e1a
家伟 <TAN18391610438@outlook.com>
2025-04-09 17:39:20 +0800

    init

---
+++ Controllers/ImagesController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AngularApi.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class ImagesController : Controller
+    {
+
+
+
+    }
+}
---
+++ Models/AngularContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AngularApi.Models
+{
+    public class AngularContext : DbContext
+    {
+
+        public AngularContext(DbContextOptions<AngularContext> options):base(options) {
+        
+        }
+
+    }
+}
